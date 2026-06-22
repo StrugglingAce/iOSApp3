5155f66208b7fe4f7c69ce3e4bc888c6f08a86d7
StrugglingAce <Chibuzor.awanye@triosstudent.com>
2026-06-22 13:45:42 -0400

    Initial Commit

---
+++ iOSApp3/iOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp3App.swift
+//  iOSApp3
+//
+//  Created by Chibuzor Emmanuel Awanye on 2026-06-22.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
